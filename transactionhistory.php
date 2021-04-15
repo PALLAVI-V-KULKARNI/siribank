bd3b248eddad771790dba66c43b032f25a996280
PALLAVI-V-KULKARNI <77944985+PALLAVI-V-KULKARNI@users.noreply.github.com>
2021-04-15 13:00:03 +0530

    Add files via upload

---
+++ transactionhistory.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transaction History</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link href="table.css" rel="stylesheet" />
+</head>
+
+<body style="background-color:peachpuff">
+
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Siri Bank</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php?id= ">Home</a>
+                    </li>
+
+
+
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <h2 class="text-center pt-4">TRANSACTION HISTORY</h2>
+
+        <br>
+        <div class="table-responsive-sm">
+            <table class="table table-hover table-striped table-condensed table-bordered border-1px solid black">
+                <thead>
+                    <tr>
+                        <th class="text-center">Sno</th>
+                        <th class="text-center">Debited from</th>
+                        <th class="text-center">Credited to</th>
+                        <th class="text-center">Amount(in Rs.)</th>
+                        <th class="text-center">Date & Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+
+                    $servername = "localhost";
+                    $username = "root";
+                    $password = "";
+                    $database = "bank";
+
+                    // Create a connection
+                    $conn = mysqli_connect($servername, $username, $password, $database);
+                    // Die if connection was not successful
+                    if (!$conn) {
+                        die("Sorry we failed to connect: " . mysqli_connect_error());
+                    }
+
+                    $sql = "select * from transaction";
+
+                    $query = mysqli_query($conn, $sql);
+
+                    while ($rows = mysqli_fetch_assoc($query)) {
+                    ?>
+
+                        <tr>
+                            <td class="py-2"><?php echo $rows['sno']; ?></td>
+                            <td class="py-2"><?php echo $rows['sender']; ?></td>
+                            <td class="py-2"><?php echo $rows['receiver']; ?></td>
+                            <td class="py-2"><?php echo $rows['balance']; ?> </td>
+                            <td class="py-2"><?php echo $rows['datetime']; ?> </td>
+
+                        <?php
+                    }
+
+                        ?>
+                </tbody>
+            </table>
+
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+
+</html>
