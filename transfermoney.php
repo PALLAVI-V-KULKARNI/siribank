3135495a7cdd6ce85d61d68ef89cbd942dc5e636
PALLAVI-V-KULKARNI <77944985+PALLAVI-V-KULKARNI@users.noreply.github.com>
2021-04-15 13:08:49 +0530

    Add files via upload

---
+++ transfermoney.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transfer Money</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    
+    <style type="text/css">
+        button {
+            transition: 1.5s;
+        }
+
+        button:hover {
+            background-color: #616C6F;
+            color: white;
+        }
+    </style>
+</head>
+
+<body style="background-color:rosybrown">
+    <div class="background"></div>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Siri Bank</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php?id= ">Home</a>
+                    </li>
+
+
+
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
+    <?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "bank";
+
+    // Create a connection
+    $conn = mysqli_connect($servername, $username, $password, $database);
+    // Die if connection was not successful
+    if (!$conn) {
+        die("Sorry we failed to connect: " . mysqli_connect_error());
+    }
+    $sql = "SELECT * FROM users";
+    $result = mysqli_query($conn, $sql);
+    ?>
+
+
+    <div class="container">
+
+
+        <h2 class="text-center pt-4">TRANSFER MONEY</h2>
+
+
+        <br>
+
+        <div class="row">
+            <div class="col">
+                <div class="table-responsive-sm">
+
+                    <table class="table table-hover table-sm table-striped table-condensed table-bordered">
+                        <thead>
+
+                            <tr>
+                                <th scope="col" class="text-center py-2">Sno</th>
+                                <th scope="col" class="text-center py-2">Account Holder Name</th>
+                                <th scope="col" class="text-center py-2">Email-Id</th>
+                                <th scope="col" class="text-center py-2">Bank Balance(in Rs.)</th>
+                                <th scope="col" class="text-center py-2">Operation</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            while ($rows = mysqli_fetch_assoc($result)) {
+                            ?>
+                                <tr>
+                                    <td class="py-2"><?php echo $rows['id'] ?></td>
+                                    <td class="py-2"><?php echo $rows['name'] ?></td>
+                                    <td class="py-2"><?php echo $rows['email'] ?></td>
+                                    <td class="py-2"><?php echo $rows['balance'] ?></td>
+                                    <td><a href="selecteduserdetail.php?id= <?php echo $rows['id']; ?>"> <button type="button" class="btn">Transact</button></a></td>
+                                </tr>
+                            <?php
+                            }
+                            ?>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
